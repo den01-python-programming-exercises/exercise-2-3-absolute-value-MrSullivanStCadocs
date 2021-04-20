209c77e57e23e6397147f995f4efe0c9f7f669f0
MrSullivan <>
2021-04-20 10:28:26 +0000

    added variable, if and else statement with print commands.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    number = int(input("Write your number: "))
+
+    if (number <0):
+      print(number * -1)
+    else:
+      print(number)
 
 if __name__ == '__main__':
     main()
